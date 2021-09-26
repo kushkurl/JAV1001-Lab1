e0b2609c7f2621fe3ef242e4773fe8d05e5aef67
unknown <kushagra.kurl@gmail.com>
2021-09-26 19:37:59 +0530

    submitting/pusing my assignment on git

---
+++ ConvertSIUnits.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+class ConvertSIUnits {
+  public static void main(String args[]) {
+    System.out.println("Welcome to the SI Unit conversion app made in JAVA!");
+    String toContinue = null;
+
+    do{
+      //initialized all the variables here
+      toContinue = null;
+      Scanner userInput = new Scanner(System.in);
+      System.out.print("Enter the value: ");
+      //to entered integer value or decimal value
+      double inputVal = userInput.nextDouble();
+      System.out.print("Enter the SI unit you want to convert: ");
+      //to be entered SI unit in string
+      String unitType = userInput.next();
+
+      //below are the if else statements to match the SI unit which user has entered
+      if(unitType.toLowerCase().trim().equals("km"))
+      //dynamically showing results for each conversion in pritn statement
+        System.out.println(inputVal + " " + unitType + " is equal to: " + (inputVal*0.621371) + " Miles");
+
+      else if(unitType.toLowerCase().trim().equals("cm"))
+        System.out.println(inputVal + " " + unitType + " is equal to: " + (inputVal*0.393701) + " inches");
+
+      else if(unitType.toLowerCase().trim().equals("kg"))
+        System.out.println(inputVal + " " + unitType + " is equal to: " + (inputVal*2.20462) + " pounds");
+
+      else if(unitType.toLowerCase().trim().equals("kb"))
+        System.out.println(inputVal + " " + unitType + " is equal to: " + (inputVal*0.001) + " mb");
+
+      //for units with alias names we can use regular expressions #regex
+      else if(unitType.toLowerCase().trim().equals("c") || unitType.toLowerCase().equals("celsius")) {
+        double toFahrenheit = (inputVal*9/5) + 32;
+        System.out.println(inputVal + " " + unitType + " is equal to: " + toFahrenheit + " fahrenheit");
+      }
+
+      else if(unitType.toLowerCase().trim().equals("joule"))
+        System.out.println(inputVal + " " + unitType + " is equal to: " + (inputVal*0.239006) + " gram calorie");
+
+      else if(unitType.toLowerCase().trim().equals("pascal"))
+        System.out.println(inputVal + " " + unitType + " is equal to: " + (inputVal/133) + " torr");
+
+      //this block will run if user has entered some other SI unit not available here
+      else {
+        System.out.println("The unit enterd " + unitType + " is incorrect or application does not have appropriate conversion mehtod available! Please enter other SI unit. To continue press 'y' ");
+        toContinue = userInput.next();
+      }
+
+      //this will check if user already wish to continue or has not made decision yet
+      if(toContinue == null) {
+        if(true) {
+          System.out.println("Do do wish to convert other unit! If yes then press 'y' key.");
+          toContinue = userInput.next();
+        }
+      }
+    }
+    while(toContinue.toLowerCase().equals("y"));
+  }
+}
